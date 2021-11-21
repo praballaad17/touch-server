30425d37afcd5c7b93cdda2ae633d99473270755
praballaad17 <54369093+praballaad17@users.noreply.github.com>
2021-11-22 04:32:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require("./startups/cors")(app);
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-        origin: "https://thetouchapp.herokuapp.com",
+        origin: ["http://localhost:3000", "https://thetouchapp.herokuapp.com"],
         methods: ["GET", "POST"],
     }
 })
